f95d750b282f427c2ae665bbead3e96099cd8cde
yokxim <yooksi53@gmail.com>
2025-02-03 14:03:55 +0900

    Initial commit

---
+++ src/main/java/org/example/spring_security_oauth2_token/SpringSecurityOAuth2TokenApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_security_oauth2_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuth2TokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOAuth2TokenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spring_security_oauth2_token/SpringSecurityOAuth2TokenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_security_oauth2_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOAuth2TokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
