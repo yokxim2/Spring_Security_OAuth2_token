1445fa6d59e86c0492ce280305ccbbd932f8280e
yokxim <yooksi53@gmail.com>
2025-02-03 14:23:20 +0900

    feat[main-controller] 루트 페이지 컨트롤러 추가

---
+++ src/main/java/org/example/spring_security_oauth2_token/controller/MainController.java
@@ -0,0 +1,16 @@
+package org.example.spring_security_oauth2_token.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String mainAPI() {
+
+        return "main route";
+    }
+}
