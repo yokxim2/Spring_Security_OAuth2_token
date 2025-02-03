7e1307bf985ec4ed3aba5ba3bc49370d3d9ef055
yokxim <yooksi53@gmail.com>
2025-02-03 14:57:51 +0900

    feat[user-dto] 유저 정보를 담는 DTO 추가

---
+++ src/main/java/org/example/spring_security_oauth2_token/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package org.example.spring_security_oauth2_token.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String role;
+    private String name;
+    private String username;
+}
